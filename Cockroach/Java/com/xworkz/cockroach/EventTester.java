bf220babe2644f821e38be792bc8706ba08de7a5
kchaitli <73681368+kchaitli@users.noreply.github.com>
2021-01-11 17:48:40 +0530

    uploaded files for creating exceptional event and handling it

---
+++ Cockroach/Java/com/xworkz/cockroach/EventTester.java
@@ -0,0 +1,12 @@
+package com.xworkz.cockroach;
+
+import com.xworkz.cockroach.event.ExceptionalEvent;
+
+public class EventTester {
+
+	public static void main(String[] args) throws Throwable {
+		ExceptionalEvent event = new ExceptionalEvent();
+		event.createExceptionalEvent(false);
+	}
+
+}
---
+++ Cockroach/Java/com/xworkz/cockroach/event/ExceptionalEvent.java
@@ -0,0 +1,19 @@
+package com.xworkz.cockroach.event;
+
+public class ExceptionalEvent {
+	public ExceptionalEvent() {
+		System.out.println("Creating ExceptionalEvent");
+	}
+	
+	public void createExceptionalEvent(boolean create) throws Throwable {
+		System.out.println("invoked createExceptionalEvent");
+		if(create) {
+			System.out.println("creating exceptional event");
+			Throwable throwable = new Throwable();
+			throw throwable;
+		}else {
+			System.out.println("no exceptional event");
+		}
+		
+	}
+}
